feba4315b986a06e91c6b001d279b5db11f7d781
Kevin Siemonsma <kevin.siemonsma@gmail.com>
2022-11-15 12:49:40 +0100

    Updated code and tailored to usecases

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ import numpy as np
 import os
 import requests
 from collections import OrderedDict
+import re
+from pathlib import Path
+
+
+def num_sort(test_string):
+    if str(test_string).isdigit():
+        out = test_string
+    else:
+        out = list(map(int, re.findall(r'\d+', test_string)))[0]
+    return out
 
 
 def ordered(d, desired_key_order):
@@ -22,12 +32,12 @@ class Meta:
         self.worldcat = worldcat
 
 
-csv = pd.read_csv('csv/delta.csv')
-Meta.title = "Delta"
+csv = pd.read_csv('csv/Owee.csv')
+Meta.title = "Oweekrant"
 Meta.author = "Delft: Technische Hogeschool"
-Meta.worldcat = '<a href="https://tudelft.on.worldcat.org/oclc/72677405">72677405</a>'
+Meta.worldcat = 'N/A'
 
-start_jaargang = 15
+start_jaargang = 1
 
 dlcs_base = "https://dlc.services/iiif-resource/7/string1string2string3/{}/{}"
 
@@ -67,14 +77,33 @@ for i, key in enumerate(groups.keys()):
     ref1 = key[0]
     ref2 = key[1]
 
-    dlcs_json = dlcs_base.format(ref1.lstrip("en-"), ref2)
-    if str(ref2).isdigit():
-        # jaar = str(ref2) + "-" + str(np.int_(ref2) + 1)[2:]
-        # jaar = str(np.int_(ref2)-1)+"-"+str(ref2)[2:]
-        if len(str(ref2)) >4:
-            jaar = str(ref2)[:4]+"-"+str(ref2)[-2:]
+    if Meta.title == 'Delft Integraal':
+        dlcs_json = dlcs_base.format(ref1.lstrip("nl-"), ref2)
+    elif Meta.title == 'Delft Outlook':
+        dlcs_json = dlcs_base.format(ref1.lstrip("en-"), ref2)
+    else:
+        dlcs_json = dlcs_base.format(ref1, ref2)
+
+    if Meta.title == 'THD Nieuws':
+        if str(ref2).isdigit():
+            # jaar = str(ref2) + "-" + str(np.int_(ref2) + 1)[2:]
+            # jaar = str(np.int_(ref2)-1)+"-"+str(ref2)[2:]
+            if len(str(ref2)) > 4:
+                jaar = str(ref2)[:4]+"-"+str(ref2)[-2:]
+            else:
+                jaar = str(np.int_(ref2) - 1) + "-" + str(ref2)[2:]
         else:
-            jaar = str(np.int_(ref2) - 1) + "-" + str(ref2)[2:]
+            jaar = ref2
+    elif Meta.title == 'TH Mededelingen':
+        if str(ref2).isdigit():
+            # jaar = str(ref2) + "-" + str(np.int_(ref2) + 1)[2:]
+            # jaar = str(np.int_(ref2)-1)+"-"+str(ref2)[2:]
+            if len(str(ref2)) > 4:
+                jaar = str(ref2)[:4]+"-"+str(ref2)[-2:]
+            else:
+                jaar = str(ref2)[2:] + "-" + str(np.int_(ref2) + 1)
+        else:
+            jaar = ref2
     else:
         jaar = ref2
     year_filename = "{}-{}.json".format(ref1.lower(), jaar)
@@ -82,7 +111,7 @@ for i, key in enumerate(groups.keys()):
     ref_id = "https://tu-delft-library.github.io/delta-archief/manifests/{}/{}"\
         .format(Meta.title.replace(" ", "-").lower(), year_filename)
     mani = {"@id": ref_id,
-            "label": jaar,
+            "label": str(jaar),
             "@type": "sc:Manifest"}
 
     json_out["manifests"].append(mani)
@@ -94,22 +123,21 @@ for i, key in enumerate(groups.keys()):
     json_req = requests.get(dlcs_json).json()
     json_req['structures'] = []
     mag_key_list = list(group_mag.keys())
-    for k, mag_key in enumerate(mag_key_list):
-        if mag_key.isdigit():
-            mag_key_list[k] = np.int_(mag_key)
-        else:
-            mag_key_list[k] = mag_key
 
-    mag_keys = np.sort(mag_key_list)
-    for j, mag in enumerate(mag_keys):
-        if len(str(mag)) == 1:
-            mag = str(mag).zfill(2)
-        index_page = group_mag[str(mag)][0]
-        ref3 = mag
-        if str(ref3).isdigit():
-            struct_label = "Nr. {}".format(ref3)
+    mag_key_list.sort(key=num_sort)
+
+    for j, mag in enumerate(mag_key_list):
+        # if len(str(mag)) == 1:
+        #     mag = str(mag).zfill(2)
+        if any(item == Meta.title for item in ['Delft Outlook', 'Oweekrant']):
+            index_page = group_mag[mag][0]
         else:
+            index_page = group_mag[str(mag)][0]
+        ref3 = mag
+        if str(ref3).isalpha():
             struct_label = ref3
+        else:
+            struct_label = "Nr. {}".format(ref3)
         # ref3 = csv.loc[group_mag[mag][0]]["Reference3"]
         structure = {
             "@id": "https://dlc.services/iiif-resource/7/string1/72820760-01/range/{}".format(j),
@@ -122,9 +150,9 @@ for i, key in enumerate(groups.keys()):
         }
         json_req['structures'].append(structure)
 
-    if int(ref2[:4]) < 1986:
+    if int(str(ref2)[:4]) < 1986:
         Meta.author = "Delft: Technische Hogeschool"
-    elif int(ref2[:4]) == 1986:
+    elif int(str(ref2)[:4]) == 1986:
         Meta.author = "Delft: Technische Hogeschool / Technische Universiteit"
     else:
         Meta.author = "Delft: Technische Universiteit"
@@ -136,7 +164,7 @@ for i, key in enumerate(groups.keys()):
         {"label": "Uitgever",
          "value": Meta.author},
         {"label": "Jaargang",
-         "value": str(i + start_jaargang)+" ("+jaar+")"},
+         "value": str(i + start_jaargang)+" ("+str(jaar)+")"},
         {"label": "Worldcat",
          "value": Meta.worldcat}
     ]
@@ -145,7 +173,7 @@ for i, key in enumerate(groups.keys()):
     json_req = ordered(json_req, desired_key_order)
 
     json_year = json.dumps(json_req, indent=8)
-
+    Path("manifests/"+Meta.title.replace(" ", "-").lower()).mkdir(parents=True, exist_ok=True)
     with open("manifests/"+Meta.title.replace(" ", "-").lower()+"/"+year_filename, "w") as outfile:
         outfile.write(json_year)
 
